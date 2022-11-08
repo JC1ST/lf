8ac2f1df61688fd711478063f1ae27085ad54593
강동연 <ehetks@gmail.com>
2022-11-08 16:46:22 +0900

    login

--- src/main/java/kr/co/lovefans/devel/service/SpringConfig.java
+++ src/main/java/kr/co/lovefans/devel/service/SpringConfig.java
@@ -1,8 +1,7 @@
 package kr.co.lovefans.devel.service;
 
 
-import kr.co.lovefans.devel.repository.CreatorRepository;
-import kr.co.lovefans.devel.repository.MemberRepository;
+import kr.co.lovefans.devel.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,21 +10,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private final SubsPostCustomRepository subsPostCustomRepository;
 
     private final MemberRepository memberRepository;
     private final CreatorRepository creatorRepository;
+    private final CreatorPostRepository creatorPostRepository;
+    private final SubListRepository subListRepository;
 
     @Autowired
-    public SpringConfig(MemberRepository memberRepository, CreatorRepository creatorRepository) {
+    public SpringConfig(SubsPostCustomRepository subsPostCustomRepository, MemberRepository memberRepository, CreatorRepository creatorRepository, CreatorPostRepository creatorPostRepository, SubListRepository subListRepository) {
+        this.subsPostCustomRepository = subsPostCustomRepository;
         this.memberRepository = memberRepository;
         this.creatorRepository = creatorRepository;
+        this.creatorPostRepository = creatorPostRepository;
+        this.subListRepository = subListRepository;
     }
-
-
-
-
-
-
 //    private DataSource dataSource;
 
 
@@ -43,7 +42,18 @@ public class SpringConfig {
     }
     @Bean
     public CreatorService creatorService(){
-        return new CreatorService(creatorRepository);
+
+        return new CreatorService(creatorRepository, memberRepository);
+    }
+    @Bean
+    public CreatorPostService creatorPostService(){
+
+        return new CreatorPostService(creatorPostRepository, subsPostCustomRepository);
+    }
+
+    @Bean
+    public SubListService subListService(){
+        return new SubListService(subListRepository);
     }
 
 //    @Bean
