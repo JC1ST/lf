1fcc35916e3f55499307b64fe5e44c2abbf5c396
강동연 <ehetks@gmail.com>
2022-11-16 10:53:02 +0900

    구독하기 구독취소

--- src/main/java/kr/co/lovefans/devel/controller/MemberControrller.java
+++ src/main/java/kr/co/lovefans/devel/controller/MemberControrller.java
@@ -1,9 +1,14 @@
 package kr.co.lovefans.devel.controller;
 
+import kr.co.lovefans.devel.domain.CreatorInfoDto;
 import kr.co.lovefans.devel.domain.Member;
+import kr.co.lovefans.devel.domain.SubListDto;
+import kr.co.lovefans.devel.domain.SubListDtoId;
 import kr.co.lovefans.devel.form.LoginForm;
 import kr.co.lovefans.devel.form.MemberForm;
+import kr.co.lovefans.devel.service.CreatorService;
 import kr.co.lovefans.devel.service.MemberService;
+import kr.co.lovefans.devel.service.SubListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +24,14 @@ import java.util.Optional;
 public class MemberControrller {
 
     private final MemberService memberService;
+    private final CreatorService creatorService;
+    private final SubListService subListService;
 
     @Autowired
-    public MemberControrller(MemberService memberService){
+    public MemberControrller(MemberService memberService, CreatorService creatorService, SubListService subListService){
         this.memberService = memberService;
+        this.creatorService = creatorService;
+        this.subListService = subListService;
     }
 
 
@@ -84,24 +93,34 @@ public class MemberControrller {
             Optional<Member> memberInfo = memberService.findById(form.getMi_id());
             System.out.println("회원닉 = " + memberInfo.get().getMiNick());
 
-            //model.addAttribute("model", memberInfo.get());
+//            model.addAttribute("model", memberInfo.get());
 
             HttpSession session = request.getSession();
             session.setAttribute("session",memberInfo.get().getMiSeq());
+            session.setAttribute("kind",memberInfo.get().getMiKind());
+            HttpSession memberInfoSession = request.getSession();
+            memberInfoSession.setAttribute("memberInfoSession",memberInfo.get());
 
-            return "redirect:mypage";
-
+            Optional<CreatorInfoDto> creatorInfo = creatorService.findOne(memberInfo.get().getMiSeq());
+            if(memberInfo.get().getMiKind().equals("C")) {
+                HttpSession creatorInfoSession = request.getSession();
+                creatorInfoSession.setAttribute("creatorInfoSession", creatorInfo.get());
+                return "redirect:/creators/creator_main?key=" + memberInfo.get().getMiSeq();
+            } else {
+                return "redirect:/subs/main?key=" + memberInfo.get().getMiSeq();
+            }
         }else {
             System.out.println("아이디 또는 비밀번호를 확인해주세요");
             model.addAttribute("message","아이디 또는 비밀번호를 확인해주세요");
-            } return "forward:/login";
-        }
+        } return "forward:/login";
+    }
 
     @GetMapping("/members/logout")
     public String logout(HttpSession session){
-        System.out.println("세션삭제" + session.getAttribute("session"));
 
-        session.removeAttribute("session");
+        session.invalidate();
+
+
 
         return "redirect:/";
 
@@ -109,25 +128,26 @@ public class MemberControrller {
 
     @GetMapping("/members/mypage")
     public String myPage(HttpSession session,Model model){
-        String go = "";
+        String go = "views/subscr/subscr_mypage";
 
         Optional<Member> memberInfo = memberService.findOne((Long) session.getAttribute("session"));
+        if (memberInfo.isEmpty()) return "redirect:/login";
+
+
         model.addAttribute("model", memberInfo.get());
 
 
+       if(memberInfo.get().getMiKind().equals("V")) {
+           go =  "views/subscr/subscr_mypage";
+       }
+       else if(memberInfo.get().getMiKind().equals("C")) go = "redirect:/creators/creator_mypage?key="+memberInfo.get().getMiSeq();
 
-        if(memberInfo.isEmpty()) {
-            go = "redirect:login";
-        }else if(memberInfo.get().getMiKind()=="V") {
+       else {
+           return "redirect:/login";
+       }
 
-            go =  "views/subscr/subscr_mypage";
-        }else if(memberInfo.get().getMiKind()=="C") {
+        return go;
 
-            go = "redirect:/creator/creator_mypage";
-        }
-
-        //return go;
-        return "views/subscr/subscr_mypage";
 
     }
 
@@ -152,6 +172,18 @@ public class MemberControrller {
     }
 
 
+    @PostMapping("/members/subsJoin")
+    public String subsJoin(@RequestParam("cSeq") Long cSeq, @RequestParam("cslSeq") Long cslSeq,HttpSession session){
+
+        SubListDto subListDto = new SubListDto();
+        subListDto.setSubListDtoId(new SubListDtoId());
+        subListDto.getSubListDtoId().setSlCMiSeq(cSeq);
+        subListDto.getSubListDtoId().setSlVMiSeq((Long) session.getAttribute("session"));
+        subListDto.getSubListDtoId().setSlCslSeq(cslSeq);
+        subListService.save(subListDto);
+        return "redirect:/subs/creatorpage/mode0/post?key="+cSeq;
+
+    }
 
 
 }
